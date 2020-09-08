48a5e49c9805eb965f4c931e4645d308c4083344
Amar-kumar068 <69798261+Amar-kumar068@users.noreply.github.com>
2020-09-08 19:54:15 +0530

    Update gitDemo.java

--- gitDemo.java
+++ gitDemo.java
@@ -3,6 +3,6 @@ Class gitDemo{
 public static void main(String args[]){
 System.out.println("Demo file for git");
 System.out.println("In Dev Branch");
-System.out.println("pusing to github");
+System.out.println("pushing to github");
 }
 }
